7e81e2fde572346546e568c350d4d7bdd38c4f81
waterinocean <ajiql@126.com>
2022-05-01 10:13:56 +0800

    first socket program, hello socket~

---
+++ socket_programming_windows/hello_client/hello_client.cpp
@@ -0,0 +1,54 @@
+﻿// hello_client.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <WinSock2.h>
+void ErrorHandling(std::string msg);
+
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData;
+	SOCKET hSocket;
+	SOCKADDR_IN servAddr;
+
+	char msg[30];
+	int strLen;
+	if (argc != 3)
+	{
+		printf("Usage: %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		ErrorHandling("WSAStartup() error!");
+
+	hSocket = socket(PF_INET, SOCK_STREAM, 0);
+	if (hSocket == INVALID_SOCKET)
+		ErrorHandling("socket() error!");
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = inet_addr(argv[1]);
+	servAddr.sin_port = htons(atoi(argv[2]));
+
+	if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+		ErrorHandling("connect() error!");
+
+	strLen = recv(hSocket, msg, sizeof(msg) - 1, 0);
+	if (strLen == -1)
+		ErrorHandling("read() error!");
+	printf("Message from server: %s \n", msg);
+
+	closesocket(hSocket);
+	WSACleanup();
+	return 0;
+}
+
+void ErrorHandling(std::string msg)
+{
+	fputs(msg.c_str(), stderr);
+	fputs("\n", stderr);
+	exit(1);
+}
---
+++ socket_programming_windows/hello_server/hello_server.cpp
@@ -0,0 +1,71 @@
+﻿// hello_server.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <WinSock2.h>
+void ErrorHandling(std::string msg);
+
+int main(int argc, char* argv[])
+{
+	WSADATA wsaData;
+	SOCKET hServSock, hClntSock;
+	SOCKADDR_IN servAddr, clntAddr;
+
+	int szClntAddr;
+	char msg[] = "Hello World!";
+	if (argc != 2)
+	{
+		printf("Usage: %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		ErrorHandling("WSAStartup() error!");
+
+	hServSock = socket(PF_INET, SOCK_STREAM, 0);
+	if (hServSock == INVALID_SOCKET)
+		ErrorHandling("socket() error!");
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servAddr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(hServSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
+		ErrorHandling("bind() error!");
+
+	if (listen(hServSock, 5) == SOCKET_ERROR)
+		ErrorHandling("listen() error!");
+
+	printf("start listen on port: %d\n", atoi(argv[1]));
+	szClntAddr = sizeof(clntAddr);
+	hClntSock = accept(hServSock, (SOCKADDR*)&clntAddr, &szClntAddr);
+	if (hClntSock == INVALID_SOCKET)
+		ErrorHandling("accept() error!");
+
+	send(hClntSock, msg, sizeof(msg), 0);
+	closesocket(hClntSock);
+	closesocket(hServSock);
+	WSACleanup();
+	return 0;
+}
+
+void ErrorHandling(std::string msg)
+{
+	fputs(msg.c_str(), stderr);
+	fputs("\n", stderr);
+	exit(1);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
